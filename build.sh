eaaf5c3a4aabf53979106d70c3cc8003e5da8a04
Viktor <35473052+viktorxda@users.noreply.github.com>
2023-10-22 13:57:00 +0200

    Update workflow for osd

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 DL="https://github.com/openipc/firmware/releases/download/latest"
 
-if [ "$1" = "vdec" ]; then
+if [ "$1" = "vdec" ] || [ "$1" = "osd" ]; then
 	CC=cortex_a7-gcc12-glibc-4_9
 else
 	CC=cortex_a7_thumb2-gcc12-musl-4_9
 fi
 
-GCC=../toolchain/$CC/bin/arm-linux-gcc
+GCC=$PWD/toolchain/$CC/bin/arm-linux-gcc
 
 if [ ! -e toolchain/$CC ]; then
 	wget -c -nv --show-progress $DL/$CC.tgz -P $PWD
@@ -21,15 +21,19 @@ if [ ! -e firmware ]; then
 fi
 
 if [ "$1" = "vdec" ]; then
-	DRV=../firmware/general/package/hisilicon-osdrv-hi3536dv100/files/lib
+	DRV=$PWD/firmware/general/package/hisilicon-osdrv-hi3536dv100/files/lib
 	make -C vdec -B CC=$GCC DRV=$DRV
 elif [ "$1" = "venc-goke" ]; then
-	DRV=../firmware/general/package/goke-osdrv-gk7205v200/files/lib
+	DRV=$PWD/firmware/general/package/goke-osdrv-gk7205v200/files/lib
 	make -C venc -B CC=$GCC DRV=$DRV $1
 elif [ "$1" = "venc-hisi" ]; then
-	DRV=../firmware/general/package/hisilicon-osdrv-hi3516ev200/files/lib
+	DRV=$PWD/firmware/general/package/hisilicon-osdrv-hi3516ev200/files/lib
 	make -C venc -B CC=$GCC DRV=$DRV $1
+elif [ "$1" = "osd" ]; then
+	cd osd
+	cmake -Bbuild -DCMAKE_C_COMPILER=$GCC -DCMAKE_BUILD_TYPE=Release
+	cmake --build build --parallel 8
 else
-	echo "Usage: $0 [vdec|venc-goke|venc-hisi]"
+	echo "Usage: $0 [vdec|venc-goke|venc-hisi|osd]"
 	exit 1
 fi
