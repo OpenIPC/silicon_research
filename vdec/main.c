2d0bc79316f1e6d0d16cf44d1565a19b6a91f48a
MarioFPV <57532232+MarioFPVdev@users.noreply.github.com>
2024-06-12 12:33:51 +0100

    Small fix
    Small fix ported from FPVue

--- vdec/main.c
+++ vdec/main.c
@@ -1014,7 +1014,7 @@ void* __MAVLINK_THREAD__(void* arg) {
             {
               mavlink_raw_imu_t imu;
               mavlink_msg_raw_imu_decode(&message, &imu);
-              osd_vars.telemetry_raw_imu = imu.temperature;
+              telemetry_raw_imu = imu.temperature;
             }
             break;
           
